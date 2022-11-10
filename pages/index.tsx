baba7e1d315a8d6c2b74980840b45cb8b41df445
Alexandr Dimov <dalexandrd@gmail.com>
2022-11-10 23:38:57 +0200

    adding components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
-export default function Home() {
-  return (
-    <h1 className="text-3x1 font-bold underline">
-     TukTuk
-    </h1>
-  )
+import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "../types";
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+
+interface IProps {
+  videos: Video[];
 }
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No videos"} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
+};
+
+export default Home;
