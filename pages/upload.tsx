a4803b781b9c67556a1ffc6576ccb77f5760dc8f
Alexandr Dimov <dalexandrd@gmail.com>
2022-11-12 10:30:24 +0200

    working on wideo page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -8,12 +8,21 @@ import { SanityAssetDocument } from "@sanity/client";
 import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
 
+import { topics } from "../utils/constants";
+import { BASE_URL } from "../utils";
+
 const Upload = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [videoAsset, setVideoAsset] = useState<
     SanityAssetDocument | undefined
   >();
   const [wrongFileType, setWrongFileType] = useState(false);
+  const [caption, setCaption] = useState("");
+  const [category, setCategory] = useState(topics[0].name);
+  const [savingPost, setSavingPost] = useState(false);
+  const { userProfile }: { userProfile: any } = useAuthStore();
+
+  const router = useRouter();
 
   const uploadVideo = async (e: any) => {
     const selectedFile = e.target.files[0];
@@ -34,9 +43,38 @@ const Upload = () => {
       setWrongFileType(true);
     }
   };
+
+  const handlePost = async () => {
+    if (caption && videoAsset?._id && category) {
+      setSavingPost(true);
+
+      const document = {
+        _type: "post",
+        caption,
+        video: {
+          _type: "file",
+          asset: {
+            _type: "reference",
+            _ref: videoAsset?._id,
+          },
+        },
+        userId: userProfile?._id,
+        postedBy: {
+          _type: "postedBy",
+          _ref: userProfile?._id,
+        },
+        topic: category,
+      };
+
+      await axios.post(`${BASE_URL}/api/post`, document);
+
+      router.push("/");
+    }
+  };
+
   return (
     <div className="flex w-full h-full absolute left-0 top:[60px] mb-10 pt-10 lg:pt-20 bg-[#f8f8f8] justify-center">
-      <div className="bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6">
+      <div className="bg-white rounded-lg xl:h-[80vh] w-[60%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6">
         <div>
           <div>
             <p className="text-2xl font-bold">Upload Video</p>
@@ -98,10 +136,45 @@ const Upload = () => {
           <label className="text-md font-medium">Caption</label>
           <input
             type="text"
-            value=""
-            onChange={() => {}}
+            value={caption}
+            onChange={(e) => {
+              setCaption(e.target.value);
+            }}
             className="rounded outline-none text-md border-2 borded-gray-200 p-2"
           />
+          <label className="text-md font-medium">Choose a category</label>
+          <select
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+            className="outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-4 rounded cursor-pointer"
+          >
+            {topics.map((topic) => (
+              <option
+                key={topic.name}
+                value={topic.name}
+                className="outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300"
+              >
+                {topic.name}
+              </option>
+            ))}
+          </select>
+          <div className="flex gap-6 mt-10">
+            <button
+              type="button"
+              className="border-gray-300 border-2 text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+              onClick={() => {}}
+            >
+              Discard
+            </button>
+            <button
+              type="button"
+              className="bg-[#f51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+              onClick={handlePost}
+            >
+              Post
+            </button>
+          </div>
         </div>
       </div>
     </div>
